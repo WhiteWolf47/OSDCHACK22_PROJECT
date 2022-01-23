5084ab95bdf30244b6d6d71aa2ae9b89a3111394
ANURAG BHANDARI <bhandari2003anurag@gmail.com>
2022-01-23 11:26:06 +0530

    flask updated

--- flask.py
+++ flask.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+from predictor import predict_with_model()
+from names import get_names()
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -11,11 +14,20 @@ def man():
 @app.route('/predict', methods=['POST'])
 def home():
     data1 = request.form['a']
-    data2 = request.form['b']
-    data3 = request.form['c']
-    data4 = request.form['d']
-    arr = np.array([[data1, data2, data3, data4]])
-    pred = model.predict(arr)
+    #data2 = request.form['b']
+    #data3 = request.form['c']
+    #data4 = request.form['d']
+    #arr = np.array([[data1, data2, data3, data4]])
+    model = tf.keras.models.load_model('./Models_simp2')
+    img_path = str(data1)
+    prediction = predict_with_model(model, img_path)
+    path_to_names = r"./test_images"
+    names = get_names(path_to_names)
+    print(f"Character's Name : {names[prediction]}
+    im = Image.open(img_path)
+    im.show()
+	
+    #pred = model.predict(arr)
     return render_template('after.html', data=pred)
 
 if __name__ == "__main__":
